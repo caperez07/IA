6a0c520dffce4eb1164310fad5a48ae9774930a2
caperez07 <carolina.perez711@gmail.com>
2024-10-23 23:04:51 -0300

    Trocando a voz do Bingo (teste)

--- BINGO/text_to_speech.py
+++ BINGO/text_to_speech.py
@@ -1,16 +1,28 @@
-from gtts import gTTS
+# from gtts import gTTS
+
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play, save, stream, Voice, VoiceSettings
 
 import os
+
+client = ElevenLabs(api_key="sk_f366eab96a7243077e4fc0184bd51ec34cd4a48c322bbb40")
 
 class TextToSpeech:
     def __init__(self, language='pt-br'):
         
         self.language = language
 
+    # def save(self, text):
+    #     myobj = gTTS(text=text, lang=self.language, slow=False)
+    #     myobj.save("answer.mp3")
+    #     os.system("start answer.mp3")
+    
     def save(self, text):
-        myobj = gTTS(text=text, lang=self.language, slow=False)
-        myobj.save("answer.mp3")
-        os.system("start answer.mp3")
+        audio = client.generate(
+        voice="River")
+        play(audio)
+        save(audio, "output.mp3")
+
 
 # mytext = 'O Gabriel Medina é muito bom!'
 
